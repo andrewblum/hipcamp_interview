850b0efdd3be2fec373a9fb09d0d03e6b269a866
Andrew Blum <andrewdblum@gmail.com>
2020-01-27 18:47:41 -0800

    tic tac toe

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,124 @@
 import React from 'react';
 import axios from 'axios';
-import Todo from './Todo.js'
+import Tile from './Tile.js'
 
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {todos : null}
-    this.renderTodos = this.renderTodos.bind(this);
+    this.state = {
+      board: null, 
+      current_player: 'X', 
+      game_over: false,
+      winner: '-'
+    }
+    this.makeMove = this.makeMove.bind(this);
   }
 
   async componentDidMount() {
-    let todos = await axios.get('/todos')
-    console.log(todos.data)
-    this.setState({todos: todos.data})
+    let board = await axios.get('/board')
+    this.setState({board: board.data})
 
-    let data = {
-      key: 'val',
-      key2: 'val2'
-    }
-    let response2 = await axios.post('/post', data)
-    console.log(response2.data['hi'])
+    // let data = {
+    //   key: 'val',
+    //   key2: 'val2'
+    // }
+    // let response2 = await axios.post('/post', data)
+    // console.log(response2.data['hi'])
   }
 
-  renderTodos() {
-    let todos = []
-    for (let key in this.state.todos) {
-      todos.push(<Todo 
-        title = {this.state.todos[key].title}
-        done = {this.state.todos[key].done}/>)
+  async makeMove(x, y) {
+    console.log(this.state.current_player)
+    console.log(x, y)
+    let new_player = 'X'
+    if (this.state.current_player =='X') {
+      new_player = 'O'
     }
-    console.log(todos)
-    return todos
+
+    const move = {
+      x,
+      y,
+      value: this.state.current_player
+    }
+
+    const moveResult = await axios.put('/board', move)
+
+    this.setState({board: moveResult.data.board, 
+      current_player: new_player,
+      game_over: moveResult.data.game_over, 
+      winner: moveResult.data.winner
+    })
+
+  }
+
+  renderBoard() {
+    return (
+      <div className='board'> 
+        <div className='row'> 
+          <Tile x={0} y={0} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={0} y={1} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={0} y={2} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>          
+        </div>
+        <div className='row'> 
+          <Tile x={1} y={0} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={1} y={1} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={1} y={2} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+        </div>
+        <div className='row'> 
+          <Tile x={2} y={0} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={2} y={1} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+          <Tile x={2} y={2} 
+            board={this.state.board} 
+            onClick={this.makeMove}> 
+          </Tile>
+        </div>
+      </div>
+    )
   }
 
   render() {
-    if (this.state.todos == null) {
+    if (this.state.board == null) {
       return (<div> Loading ... </div>)
+    }
+
+    if (this.state.game_over) { 
+      return ( 
+        <div>
+          game over screen! 
+          the winner is: { this.state.winner }
+        </div>
+      )
     }
 
     return (
       <div className="App">
-        {this.renderTodos()}
+        Current player: {this.state.current_player}
+        { this.renderBoard() }
       </div>
     )
   }
--- server/server.py
+++ server/server.py
@@ -67,6 +67,74 @@ def delete_todo(id):
     else:
         return 'Invalid', 400
 
+board = [['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']]
+
+@app.route('/board')
+def get_board():
+    return jsonify(board)
+
+def is_game_over(): 
+    winner = '-'
+    for row in range(3):
+        val = board[row][0] + board[row][1] + board[row][2] 
+        if val == 'XXX': 
+            winner = 'X'
+        if val == 'OOO':
+            winner = 'O'
+    for col in range(3):
+        val = board[0][col] + board[1][col] + board[2][col] 
+        if val == 'XXX':
+            winner = 'X'
+        if val == 'OOO':
+            winner = 'O'
+    val = board[0][0] + board[1][1] + board[2][2] 
+    if val == 'XXX':
+        winner = 'X'
+    if val == 'OOO':
+        winner = 'O'
+    val = board[0][2] + board[1][1] + board[2][0] 
+    if val == 'XXX':
+        winner = 'X'
+    if val == 'OOO':
+        winner = 'O'
+    return winner 
+
+
+    
+
+@app.route('/board', methods=['PUT'])
+def update_board():
+    
+    data = request.get_json()
+    print('got here')
+    print(data)
+    x = int(data.get('x'))
+    y = int(data.get('y'))
+    value = data.get('value')
+
+    if x < 0 or x > 2 or y < 0 or y > 2:
+        return 'Invalid Move', 400
+    if value != 'X' and value != 'O':
+        return 'Invalid Move', 400
+    if board[x][y] != '-':
+        return 'Invalid Move', 400
+    board[x][y] = value
+
+    game_over = False
+    winner = is_game_over()
+    if winner != '-':
+        game_over = True
+    
+    data = {
+        'board': board,
+        'game_over': winner != '-',
+        'winner': winner
+    }  
+
+    return jsonify(data)
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0")
