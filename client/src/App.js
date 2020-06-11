1af28f062647fa7c0290558a84018236ab3de131
Andrew Blum <andrewdblum@gmail.com>
2020-06-10 18:11:35 -0700

    remove this.state sets and checks

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Tile from './Tile.js'
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      board: null, 
-      current_player: 'X', 
-      game_over: false,
-      winner: ' '
-    }
-    this.makeMove = this.makeMove.bind(this);
-    this.restart = this.restart.bind(this);
-  }
+function App() {
 
-  async restart() {
+  const [board, setBoard] = useState(null);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [gameOver, setGameOver] = useState(false);
+  const [winner, setWinner] = useState(' ');
+
+  async function restart() {
     let board = await axios.post('/restart')
-    this.setState({board: board.data})
+    setBoard(board.data)
   }
 
-  async componentDidMount() {
+  useEffect(() => {
     let board = await axios.get('/board')
-    this.setState({board: board.data})
+    setBoard(board.data)
 
     // let data = {
     //   key: 'val',
@@ -32,30 +26,25 @@ class App extends React.Component {
     // }
     // let response2 = await axios.post('/post', data)
     // console.log(response2.data['hi'])
-  }
+  });
 
   async makeMove(x, y) {
-    console.log(this.state.current_player)
-    console.log(x, y)
     let new_player = 'X'
-    if (this.state.current_player =='X') {
+    if (current_player =='X') {
       new_player = 'O'
     }
 
     const move = {
       x,
       y,
-      value: this.state.current_player
+      value: current_player
     }
 
     const moveResult = await axios.put('/board', move)
-
-    this.setState({board: moveResult.data.board, 
-      current_player: new_player,
-      game_over: moveResult.data.game_over, 
-      winner: moveResult.data.winner
-    })
-
+    setBoard(moveResult.data.board)
+    setCurrentPlayer(new_player)
+    setGameOver(moveResult.data.game_over)
+    setWinner(moveResult.data.winner)
   }
 
   renderBoard() {
@@ -63,43 +52,43 @@ class App extends React.Component {
       <div className='board'> 
         <div className='row row-1'>
           <Tile x={0} y={0} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={0} y={1} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={0} y={2} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>          
         </div>
         <div className='row row-2'> 
           <Tile x={1} y={0} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={1} y={1} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={1} y={2} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
         </div>
         <div className='row row-3'> 
           <Tile x={2} y={0} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={2} y={1} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
           <Tile x={2} y={2} 
-            board={this.state.board} 
+            board={board} 
             onClick={this.makeMove}> 
           </Tile>
         </div>
@@ -107,30 +96,28 @@ class App extends React.Component {
     )
   }
 
-  render() {
-    if (this.state.board == null) {
-      return (<div> Loading ... </div>)
-    }
+  if (board == null) {
+    return (<div> Loading ... </div>)
+  }
 
-    if (this.state.game_over) { 
-      return ( 
-        <div>
-          game over screen! 
-          the winner is: { this.state.winner }
-        </div>
-      )
-    }
-
-    return (
-      <div className="App">
-        Current player: {this.state.current_player}
-        { this.renderBoard() }
-        <button onClick={this.restart} className='restart'> 
-          Restart 
-        </button>
+  if (game_over) { 
+    return ( 
+      <div>
+        game over screen! 
+        the winner is: { winner }
       </div>
     )
   }
+
+  return (
+    <div className="App">
+      Current player: {current_player}
+      { this.renderBoard() }
+      <button onClick={this.restart} className='restart'> 
+        Restart 
+      </button>
+    </div>
+  )
 
 }
 
